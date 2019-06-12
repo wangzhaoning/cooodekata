06fa194887c2efb9e38035f7d032148da77ca9e6
wangzhaoning <1078674571@qq.com>
2019-06-12 22:28:35 +0800

    add 20

---
+++ src/kata/leet/DicesSum.java
@@ -0,0 +1,17 @@
+package kata.leet;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wangzn
+ * @date 2019Äê6ÔÂ12ÈÕ  
+ */
+public class DicesSum {
+	 public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+		return null;
+	        // Write your code here
+	        // Ps. new AbstractMap.SimpleEntry<Integer, Double>(sum, pro)
+	        // to create the pair
+	    }
+}
