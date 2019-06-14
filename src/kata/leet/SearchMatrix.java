aa7ea8680f67fdc5e3ca5b8c63ec927499d048a1
wangzhaoning <1078674571@qq.com>
2019-06-14 23:47:04 +0800

    add 28..

--- src/kata/leet/SearchMatrix.java
+++ src/kata/leet/SearchMatrix.java
@@ -5,5 +5,12 @@ package kata.leet;
  * @date 2019Äê6ÔÂ14ÈÕ  
  */
 public class SearchMatrix {
-
+	public boolean searchMatrix(int[][] matrix, int target) {
+		int row=matrix.length;
+		int col=matrix[0].length;
+		if(target<matrix[row/2][row/2]) {
+			
+		}
+	return false;
+	}
 }
