d1c27da1d4a6a431f321867e00c834fd2c4c7530
qinxiaoyu <1078674571@qq.com>
2019-06-21 09:59:38 +0800

    fix threadpool

--- src/kata/practice/TestNewFixedThreadPool.java
+++ src/kata/practice/TestNewFixedThreadPool.java
@@ -35,7 +35,7 @@ public class TestNewFixedThreadPool {
              	       props.setProperty("user", "root");
              	        props.setProperty("sslmode", "disable");
              	        	Connection db = DriverManager
-             	        			.getConnection("jdbc:postgresql://10.221.129.149:26257/xsmlogin", props);
+             	        			.getConnection("jdbc:postgresql://xxxx:xxxx/xsmlogin", props);
              	        	tanslation(db);
              	    endTime = System.currentTimeMillis();
              	  System.out.println("程序运行时间： "+(endTime-startTime)+"ms"); 
@@ -55,37 +55,6 @@ public class TestNewFixedThreadPool {
 			i++;
 			System.out.println("welcome user");
 			listNums.add(1);
-			 ResultSet res = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='0' AND PARMS_NAME='NO_IMPRESSIBLE_PARAMETER'");
-			 ResultSet res1 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='0' AND PARMS_NAME='NO_IMPRESSIBLE_PARAMETER'");
-			 ResultSet res3 = db.createStatement()
-			            .executeQuery("SELECT INNER_OPEN_ID,LOCK_TIME,PSWD_EXPIRED_TIME,MENG_ID,USER_ID ,NICKNAME,USER_TYPE, PASSWORD,IS_MRB,a.COM_ID,SALEDPT_ID,REF_ID,LOGIN_EMAIL,VERIFIED_MAIL,LOGIN_PHONE,VERIFIED_PHONE,NOTIFY_LEVEL,  B.COM_NAME,B.COM_SHORT,B.TYPE AS COM_TYPE,B.PARENT_COM_ID,DOMAIN_URL from ST_USERS A,ST_COM B WHERE A.COM_ID=B.COM_ID AND USER_ID = "+"'"+username+"'"+"\n" + 
-			            		"");
-			 ResultSet res4 = db.createStatement()
-			            .executeQuery("SELECT COM_ID, USER_ID, DOMAIN_URL, IS_IOP_USER FROM ST_USER_PREURL WHERE USER_ID = "+"'"+username+"'");
-			 ResultSet res5 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='CONTROL_ADMIN_LOGIN_IP'");
-			 ResultSet res6 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='FORBIDDEN_UNSAFE_PASSWORD'");
-			 ResultSet res7 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='FORBIDDEN_PASSWORD'");
-			 ResultSet res8 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='DUAL_FACTOR_AUTH_ISMG'");
-			 ResultSet res9 = db.createStatement()
-			            .executeQuery("SELECT UID,USER_KEY,USER_VALUE FROM USER_PROPERTY WHERE UID='11610201#"+username+"'"+" AND USER_KEY='DUAL_FACTOR_AUTH_PHONE'");
-			 ResultSet res10 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='0' AND PARMS_NAME='XSM_DOMAIN'");
-			 ResultSet res11 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='GRAY_SCALE'");
-			 ResultSet res12 = db.createStatement()
-			            .executeQuery("SELECT UID,USER_KEY,USER_VALUE FROM USER_PROPERTY WHERE USER_PROPERTY.UID = "+"'"+username+"'");
-			 ResultSet res13 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='XSM_VERSION'");
-			 ResultSet res14 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='IS_PWD_EXPIRED_ALERT'");
-			 ResultSet res15 = db.createStatement()
-			            .executeQuery("SELECT PARMS_VALUE FROM ST_ORGAN_PARMS WHERE ORGAN_ID='11610201' AND PARMS_NAME='IS_NEED_V6'");
 		}catch(Exception e){
 			System.out.println(e);
 		}finally {
